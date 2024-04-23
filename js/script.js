4a0ca7007b213270a345a4faec763f85bf52226b
LeoPiriz1 <pirizleo1@gmail.com>
2024-04-22 21:01:51 -0300

    preentrega2

--- js/script.js
+++ js/script.js
@@ -1,28 +1,39 @@
-let numeroIngresado
-let numero
+function Persona(nombre, edad) {
+    this.nombre = nombre;
+    this.edad = edad;
+  }
+  
 
-while (true) {
-    numeroIngresado = prompt("Por favor ingrese un número:")
-
+  let personas = [
+    new Persona("Juan", 15),
+    new Persona("María", 18),
+    new Persona("Pedro", 14),
+    new Persona("Ana", 12),
+    new Persona("Luis", 17),
+    new Persona("Sofía", 11),
+    new Persona("Diego", 16),
+    new Persona("Carla", 13),
+    new Persona("Mateo", 19),
+    new Persona("Valentina", 20)
+  ];
+  
+  
+  let filtroEdad = parseInt(prompt("Ingrese la edad para filtrar personas (10 a 20 años) "));
+  
+  
+  let personasFiltradas = personas.filter(persona => persona.edad === filtroEdad);
+  
+  
+  if (personasFiltradas.length > 0) {
     
-    if (!isNaN(numeroIngresado)) {
-        numero = parseInt(numeroIngresado)
-        break; 
-    } else {
-        alert("El valor ingresado no es un número válido. Inténtelo de nuevo.")
-    }
-}
+    let mensaje = "Personas encontradas con la edad " + filtroEdad + ":\n";
+    personasFiltradas.forEach(persona => {
+      mensaje += "- " + persona.nombre + "\n";
+    });
+    
+    alert(mensaje);
+  } else {
+    alert("No se encontraron personas con la edad ingresada.");
+  }
 
-function esPar(numero) {
-    if (numero % 2 === 0) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-if (esPar(numero)) {
-    alert(numero + " es un número par.")
-} else {
-    alert(numero + " es un número impar.")
-}
+ 
